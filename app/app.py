043a040c1ab70cce6b8c074b3517a566e94cca57
jinfeng meng <45581414+mengjinfeng0331@users.noreply.github.com>
2019-04-18 22:00:06 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -38,36 +38,33 @@ def dashboard():
         return 'Error with your input, please check!'
     
     ## retrieve user input from session
-    userinput = session.pop('userinput',None)
+    data = session.pop('userinput',None)
 
     ## convert user input into Dataframe
-    inputdf= pd.DataFrame.from_dict(userinput,orient='index').T
+    inputdf= pd.DataFrame.from_dict(data,orient='index').T
     
-    ## check if the loan is approved or not
-    approval = modelAPI.getApproval(inputdf)
-
+    
     ## initialize data    
     data['approval'] = modelAPI.getApproval(inputdf)
     data['interest'] = modelAPI.getInterestRate(inputdf)
     data['Credit_Score'] = modelAPI.getCreditScore(inputdf)
-    
     if data['approval'] == 'true':
         data['alt1_amt']='none'
-        data['alt1_term']='none'
+        data['alt1_rate']='none'
         data['alt2_amt']='none'
         data['alt2_rate']='none'
+#        return str(data)
     else:
         alternate =modelAPI.getAlternative(inputdf)
         data['alt1_amt']=alternate[0]
         data['alt1_rate']=alternate[1]
         data['alt2_amt']=alternate[2]
         data['alt2_rate']=alternate[3]
-        
+#    return str(data)
     return render_template('results_page.html',data=data)
 
 def ParseUserInput(form):
     ''' parse user intput into right format, output the dictionary for ML model
-        
         Input: 
             form: from Userinput
         Output:
