a67f1e964ee39349e4ab943953e2a75e9151d0e1
jinfeng meng <45581414+mengjinfeng0331@users.noreply.github.com>
2019-04-19 22:15:44 -0400

    Update employment.js

--- app/static/js/employment.js
+++ app/static/js/employment.js
@@ -1,4 +1,4 @@
-var fileName = "./employment_summary_ALL.csv";
+var fileName = "./static/csv/employment_summary_ALL.csv";
 var employmentyears = ["< 1 year", "1 year", "2 years", "3 years", 
                         "4 years", "5 years", "6 years", "7 years",     
                         "8 years", "9 years", "10+ years"];
